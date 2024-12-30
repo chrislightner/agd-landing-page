a11a7af685e4c4a57207f2b5d65b3bb62eeaef51
Chris Lightner <chrislightner@Lightner-MacBook-Pro.local>
2024-12-30 09:49:17 -0600

    updated commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# AGD Insurance Landing Page
+
+This site uses W3 html includes. In order to work on this locally, you must use a local dev server running php, such as MAMP, and view it via localhost. Just previewing in a browser won't execute the includes.
+
+You will also need a sass compiler like [Koala](http://koala-app.com/)
+
+This site uses Bootstrap.
+
+This site is hosted at Heroku.
+
+---
+
+## When committing changes, please make two pushes.
+
+## https://www.myagdinsurance.com/
+
+### First push to our canonical "origin" repository on Bitbucket
+
+``git push``  
+ or  
+``git push origin master``
+
+### Secondly, push to the Heroku server
+
+``git push heroku master``
+
+---
+
+## This site doesn't actually use PHP
+
+In order to "trick" Heroku into serving this static site (it is not an app), we pretend it's a php app. We do this with two files:
+
+``composer.json`` Although this file is basically empty, it tells Heroku that this is a php app and to look for an index.php file
+
+``index.php`` There is no php code to execute, but Heroku will be looking for an index.php page - so we use this as our home page
+
+---
+
+## Redeploy to Heroku 20 10/14/21
---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<!-- saved from url=(0053)https://getbootstrap.com/docs/4.3/examples/jumbotron/ -->
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description"
+    content="Choose AGD insurance plans for dentists, including disability income, term life, accident disability, business overhead expense and more.">
+  <title>AGD Insurance for dentists</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  <style>
+    .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+    }
+
+    @media (min-width: 768px) {
+      .bd-placeholder-img-lg {
+        font-size: 3.5rem;
+      }
+    }
+  </style>
+  <!-- Custom styles for this template -->
+  <link href="css/jumbotron.css" rel="stylesheet">
+
+
+  <!-- Custom styles for AGD -->
+  <link href="css/styles.css" rel="stylesheet">
+
+  <!-- Google Tag Manager -->
+  <script>
+    (function (w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-5RHQPFJ');
+  </script>
+  <!-- End Google Tag Manager -->
+
+</head>
+
+<body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5RHQPFJ" height="0" width="0"
+      style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+  <div w3-include-html="includes/nav.htm"></div>
+
+  <main id="main">
+
+    <div w3-include-html="includes/jumbotron.htm"></div>
+
+    <a id="coverages"></a>
+
+    <div class="container-fluid banner text-center mb-3">
+      <h2>Insurance Coverage for AGD Members</h2>
+    </div>
+
+    <div class="container coverages-two">
+      <div class="text-center">
+        <p>Choose a coverage to view costs, plan features, eligibility, limitations and exclusions.</p>
+      </div>
+      <div class="row">
+        <div class="col-lg bg-blue">
+          <div class="h2">
+            <div>Group Life Insurance</div>
+          </div>
+          <p>Select your monthly premium that’s right for you and add to your family’s life insurance protection.
+            Includes the option to lock in your rate for 10&nbsp;years.</p>
+          <p><a class="btn btn-outline-light" href="https://coveragedetails.net/agd/life/5084" role="button"
+              target="_blank" aria-label="View Group Life Insurance Details">View
+              details »</a></p>
+        </div>
+        <div class="col-lg bg-blue">
+          <div class="h2">
+            <div>Group Disability Income Insurance</div>
+          </div>
+          <p>Strengthen your disability coverage with up to $10,000 in monthly income tax-free benefits to use any way
+            you
+            want. See your cost in seconds, online.</p>
+          <p><a class="btn btn-outline-light" href="https://coveragedetails.net/agd/di/5083" role="button"
+              target="_blank" aria-label="View Group Disability Income Insurance Details">View details
+              »</a></p>
+        </div>
+        <div class="col-lg bg-blue">
+          <div class="h2">
+            <div>Cyber Enterprise Risk Management</div>
+          </div>
+          <p>Insurance and risk mitigation solutions offer protection in case your dental practice experiences a cyber
+            incident such as data breach or electronic theft.</p>
+          <p><a class="btn btn-outline-light" href="https://marketing.seedpodcyber.com/agdsales" role="button" aria-label="View Cyber Enterprise Risk Management Details" target="_blank">View details »</a></p>
+        </div>
+        <div class="col-lg bg-blue">
+          <div class="h2">
+            <div>Small Group Health Insurance Coverage</div>
+          </div>
+          <p>Compare top health insurance plans, including easy-to-understand network and coverage information. Find
+            your best option and get an instant quote.</p>
+          <p><a class="btn btn-outline-light" href="smallgrouphealth.htm" role="button" aria-label="View Small Group Health Insurance Coverage Details">View details »</a></p>
+        </div>
+        <div class="col-lg bg-blue">
+          <div class="h2">
+            <div>Group Business Overhead Expense Insurance</div>
+          </div>
+          <p>Up to $15,000 a month to help cover your practice’s overhead costs for up to two years if you’re disabled
+            due to a covered accident or sickness.</p>
+          <p class=""><a class="btn btn-outline-light" href="boe.htm" role="button" aria-label="View Group Business Overhead Expense Insurance Details">View details »</a></p>
+        </div>
+        <div class="col-lg bg-blue">
+          <div class="h2">
+            <div>Additional Insurance</div>
+          </div>
+          <p>More information about Long-Term Care Insurance, Group Home and Auto Insurance and Group Accidental
+            Death&nbsp;&
+            Dismemberment Insurance for
+            AGD members.</p>
+          <p class=""><a class="btn btn-outline-light" href="additional.htm" role="button" aria-label="View Additional Insurance Details">View details »</a></p>
+        </div>
+      </div>
+    </div> <!-- /container -->
+
+  </main>
+
+  <div w3-include-html="includes/footer.htm"></div>
+
+  <!-- <script src="scripts/jquery-3.3.1.slim.min.js"></script> -->
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+  <script src="scripts/bootstrap.bundle.min.js"></script>
+  <script src="scripts/includeHTML.js"></script>
+  <script>
+    includeHTML();
+  </script>
+  <script>
+    setTimeout(function () {
+
+      $(document).ready(function () {
+        // Add smooth scrolling to all links
+        $("a").on('click', function (event) {
+          console.log('click');
+          // Make sure this.hash has a value before overriding default behavior
+          if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+              scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+              // Add hash (#) to URL when done scrolling (default click behavior)
+              window.location.hash = hash;
+            });
+          } // End if
+        });
+      });
+
+
+    }, 1000);
+  </script>
+</body>
+
+</html>
